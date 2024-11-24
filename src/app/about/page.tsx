33e08a2b141ab74c929081ee2ba0cf98f37cd7dc
michael-ambel <michaelambel46@gmail.com>
2024-11-24 20:48:37 +0300

    Next with TS installed

---
+++ src/app/about/page.tsx
@@ -0,0 +1,28 @@
+const About = () => {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
+        architecto modi quas placeat vel autem commodi beatae inventore quae?
+        Modi.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam
+        voluptates temporibus atque sapiente veritatis consequuntur reiciendis
+        totam non? Dignissimos, quo ipsam? Est quo rem, aut praesentium
+        repellendus inventore! Quis deleniti sit libero, iste ipsum perferendis
+        sint quia et consequuntur quo velit expedita nemo perspiciatis veritatis
+        tempore illum. Facilis nostrum laboriosam repudiandae aliquid ullam
+        eaque dolor eveniet porro voluptas enim soluta architecto laborum
+        voluptates modi, fugit perspiciatis similique. Esse reprehenderit
+        mollitia quos dolorum saepe tempora repellat ea laboriosam ullam. Quae
+        consequuntur illum amet cupiditate maxime sed velit. Quis sint libero
+        quas ad dolorem consequuntur, enim earum molestias nisi perspiciatis
+        magnam obcaecati?
+      </p>
+    </main>
+  );
+};
+
+export default About;
